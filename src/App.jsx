cf715511a4bc2399d3d9c435ba5742ba436b7384
Muhaz Ahmed <muhazvla313@gmail.com>
2023-11-09 14:44:08 +0530

    Inintial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button';
+
+const App = () => {
+  return (
+    <div>
+      <Button variant="primary">Primary</Button>
+      <h1>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
