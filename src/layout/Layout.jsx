619cae712f551f6c16369033789a51c939116d75
Muhaz Ahmed <muhazvla313@gmail.com>
2023-11-10 19:50:42 +0530

    added about

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import TopNavbar from '../components/navbar/Navbar'
 import Home from '../components/home/Home'
+import About from '../components/about/About'
+import Skills from '../components/skills/Skills'
 
 const Layout = () => {
   return (
-    <>
+    <div className='h-100'>
         <TopNavbar/>
         <Home/>
-    </>
+        <About/>
+        <Skills/>
+    </div>
   )
 }
 
