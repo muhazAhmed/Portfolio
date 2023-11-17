fedfc3b6a03002f701fd1427f622d0bd13ef0866
Muhaz Ahmed <muhazvla313@gmail.com>
2023-11-17 11:05:51 +0530

    added api for message, loader and more

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,16 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import "./footer.css";
 import { Button, Card } from "react-bootstrap";
+import Loading from "../../layout/Loading";
+import axios from "axios";
+import { URL } from "../../admin/api/API_URL";
 
 const Contact = () => {
   const email = "muhazvla313@gmail.com";
+  const [modal, setModal] = useState(false);
+  const [toast, setToast] = useState(false);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [inputs, setInputs] = useState({
+    email: "",
+    message: "",
+  });
 
   const handleEmailClick = () => {
     window.location.href = `mailto:${email}`;
   };
 
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setInputs((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const closeModal = () => {
+    setLoading(false);
+    setModal(false);
+    setError(null);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await axios.post(URL, inputs);
+      if (res.status === 201) {
+        setLoading(false);
+        return setModal(true);
+      }
+      return setLoading(false);
+    } catch (err) {
+      if (err?.response?.status === 400 || err?.response?.status === 500) {
+        console.log(err?.response?.data);
+        setTimeout(() => {
+          setToast(false);
+          setError(null);
+        }, 3000);
+        setError(err?.response?.data);
+        setToast(true);
+      }
+      return setLoading(false);
+    }
+  };
+
   return (
     <div className="contact d-flex align-items-center" id="contact">
+      {loading && <Loading />}
       <div
         className="d-flex flex-column align-items-sm-start"
         style={{ gap: "1rem" }}
@@ -57,11 +104,45 @@ const Contact = () => {
       <Card data-aos="zoom-in">
         <Card.Body>
           <Card.Title>Send me a message</Card.Title>
-          <input type="email" autoComplete="off" placeholder="Email" required />
-          <textarea placeholder="Your Message" />
+          <input
+            name="email"
+            onChange={handleChange}
+            type="email"
+            autoComplete="off"
+            placeholder="Email"
+            required
+          />
+          <textarea
+            name="message"
+            placeholder="Your Message"
+            onChange={handleChange}
+          />
         </Card.Body>
-        <Button>Submit</Button>
+        <Button onClick={handleSubmit}>Submit</Button>
       </Card>
+
+      {modal && (
+        <div className="blur">
+          <div className="modal">
+            <i className="fa-solid fa-circle-check fa-beat-fade"></i>
+            <h4>
+              You'r request
+              <br />
+              has been submitted.
+            </h4>
+            <Button variant="info" onClick={closeModal}>
+              Back
+            </Button>
+          </div>
+        </div>
+      )}
+
+      {toast && (
+        <div className="toast" data-aos="slide-left">
+          <i className="fa-solid fa-circle-xmark"></i>
+          <h4>{error ? error : "Something Went Wrong"}</h4>
+        </div>
+      )}
     </div>
   );
 };
--- src/layout/Loading.jsx
+++ src/layout/Loading.jsx
@@ -3,20 +3,19 @@ import { Audio } from "react-loader-spinner";
 
 const Loading = () => {
   return (
-    <div
-      className="w-100 d-flex align-items-center justify-content-center flex-column"
-      style={{ height: "100dvh", gap: "1rem" }}
-    >
-      <Audio
-        height="100"
-        width="100"
-        radius="10"
-        color="#AAFCEE"
-        ariaLabel="three-dots-loading"
-        wrapperStyle
-        wrapperClass
-      />
-      <h3>Loading...</h3>
+    <div className="blur">
+      <div className="loader">
+        <Audio
+          height="100"
+          width="100"
+          radius="10"
+          color="#AAFCEE"
+          ariaLabel="three-dots-loading"
+          wrapperStyle
+          wrapperClass
+        />
+        <h3>Loading...</h3>
+      </div>
     </div>
   );
 };
