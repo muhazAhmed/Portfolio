564fff655d14cb4caf163ec9d88ffd75858f70aa
muhazAhmed <muhazvla313@gmail.com>
2024-11-12 21:46:29 +0530

    added api call for initial load

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
+"use client";
 import Contact from "@/components/Contact";
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
 import Services from "@/components/Services";
 import Skills from "@/components/Skills";
+import { loadInitialServer } from "@/lib/utils";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    loadInitialServer();
+  }, []);
+
   return (
     <div className="sm:mb-11 md:mb-0">
       <div id="home">
